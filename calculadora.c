33ccaaccf74df09110ab0bd767bc3d1b11415c86
TH <155742254+aguiarth@users.noreply.github.com>
2024-08-27 16:14:55 -0300

    Q1 b) decimal para octal

--- calculadora.c
+++ calculadora.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 
 void decimal_para_binario(int numero);
-// void decimal_para_octal(int numero);
+void decimal_para_octal(int numero);
 // void decimal_para_hexadecimal(int numero);
 // void decimal_para_bcd(int numero);
 
@@ -13,15 +13,15 @@ int main(){
     scanf("%d", &numero);
     
     decimal_para_binario(numero);
+    decimal_para_octal(numero);
 
     return 0;
 }
 
 void decimal_para_binario(int numero) {
-    // conversão base decimal para base binária
-    // funciona apenas para valores inteiros positivos
+    // função apenas para valores inteiros positivos
 
-    int resto, i = 0;
+    int resto, i = 0, valorInicial = numero;
     char binario[33]; // limite para sistemas de 32 bits
 
     if (numero == 0) {
@@ -38,9 +38,41 @@ void decimal_para_binario(int numero) {
     // finalizar a string
     binario[i] = '\0';
 
+    printf("Decimal %d na base 2: ", valorInicial);
+
     // inverter a string
     for (int j = i - 1; j >= 0; j--) {
         printf("%c", binario[j]);
+    }
+    printf("\n");
+
+}
+
+void decimal_para_octal(int numero) {
+    // apenas valores inteiros positivos
+
+    int resto, i = 0, valorInicial = numero;
+    char octal[12]; // maior tamanho para inteiros de 32 bits
+
+    if (numero == 0) {
+        octal[i++] = '0';
+    }
+    
+    // método da divisão
+    while (numero > 0) {
+        resto = numero % 8;
+        numero /= 8;
+        octal[i++] = resto + '0';
+    }
+
+    // finalizar a string
+    octal[i] = '\0';
+
+    printf("Decimal %d na base 8: ", valorInicial);
+
+    // inverter a string
+    for (int j = i - 1; j >= 0; j--) {
+        printf("%c", octal[j]);
     }
     printf("\n");
 
