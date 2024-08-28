3cf1aafe3ac442ea8e351a822da410cd4eb91c00
TH <155742254+aguiarth@users.noreply.github.com>
2024-08-28 17:48:19 -0300

    1 a) e b) no formato didático

--- calculadora.c
+++ calculadora.c
@@ -24,23 +24,25 @@ void decimal_para_binario(int numero) {
     int resto, i = 0, valorInicial = numero;
     char binario[33]; // limite para sistemas de 32 bits
 
+    // para o decimal 0, o binário é 0
     if (numero == 0) {
         binario[i++] = '0';
     }
     
-    // método da divisão
+    // método da divisão - conversão decimal para binário
     while (numero > 0) {
         resto = numero % 2;
+        printf("Dividindo %d por 2: quociente = %d, resto = %d\n", numero, numero / 2, resto);
         numero /= 2;
         binario[i++] = resto + '0'; // converter o valor numérico '0' ou '1' em ASCII para representação em string
     }
 
     // finalizar a string
     binario[i] = '\0';
-
+    printf("O resultado é o último quociente seguido dos restos - leitura invertida.\n");
     printf("Decimal %d na base 2: ", valorInicial);
 
-    // inverter a string
+    // resultado - inverter a string
     for (int j = i - 1; j >= 0; j--) {
         printf("%c", binario[j]);
     }
@@ -54,20 +56,22 @@ void decimal_para_octal(int numero) {
     int resto, i = 0, valorInicial = numero;
     char octal[12]; // maior tamanho para inteiros de 32 bits
 
+    // para o decimal 0, o octal é 0
     if (numero == 0) {
         octal[i++] = '0';
     }
     
-    // método da divisão
+    // método da divisão - conversão decimal para octal
     while (numero > 0) {
         resto = numero % 8;
+        printf("Dividindo %d por 8: quociente = %d, resto = %d\n", numero, numero / 8, resto);
         numero /= 8;
         octal[i++] = resto + '0';
     }
 
     // finalizar a string
     octal[i] = '\0';
-
+    printf("O resultado é o último quociente seguido dos restos - leitura invertida.\n");
     printf("Decimal %d na base 8: ", valorInicial);
 
     // inverter a string
